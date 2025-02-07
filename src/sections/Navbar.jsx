91ca8285d884b3b42857b278307baa7787960817
bhuvan <bhuvanannappa@gmail.com>
2025-02-07 14:51:48 +0530

    small changes to the grid

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,30 +1,41 @@
 import React, { useState } from "react";
 import { navLinks } from "../constants/index.js";
+import useSmoothScroll from "../Components/UseSmoothScroll";
 
-const NavItems = ({ onClick = () => {} }) => (
-  <ul className="nav-ul">
-    {navLinks.map((item) => (
-      <li key={item.id} className="nav-li">
-        <a href={item.href} className="nav-li_a" onClick={onClick}>
-          {item.name}
-        </a>
-      </li>
-    ))}
-  </ul>
-);
+const NavItems = ({ onClick = () => {} }) => {
+  const scrollTo = useSmoothScroll();
+
+  return (
+    <ul className="nav-ul">
+      {navLinks.map((item) => (
+        <li key={item.id} className="nav-li">
+          <a
+            href={item.href}
+            className="nav-li_a"
+            onClick={(e) => {
+              e.preventDefault();
+              scrollTo(item.href.substring(1));
+              onClick();
+            }}
+          >
+            {item.name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header
-      className="fixed top-0 left-0 right-0 z-50 bg-black/90
-    "
-    >
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
       <div className="max-w-7xl mx-auto">
         <div className="flex items-center justify-between py-5 mx-auto c-space">
           <a
             href="/"
-            className=" text-neutral-400 font-bold text-xl hover:text-white transition-colors"
+            className="text-neutral-400 font-bold text-xl hover:text-white transition-colors"
           >
             Bhuvan
           </a>
@@ -35,7 +46,7 @@ const Navbar = () => {
           >
             <img
               src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
-              alt="toggel"
+              alt="toggle"
               className="w-6 h-6"
             />
           </button>
@@ -47,7 +58,7 @@ const Navbar = () => {
 
       <div className={`nav-sidebar ${isOpen ? "max-h-screen" : "max-h-0"}`}>
         <nav className="p-5">
-          <NavItems />
+          <NavItems onClick={() => setIsOpen(false)} />
         </nav>
       </div>
     </header>
