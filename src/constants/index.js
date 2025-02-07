54a2b448fd13cec6ac764ed172524aa88c694e03
bhuvan <bhuvanannappa@gmail.com>
2025-02-07 17:57:58 +0530

    added all data

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,11 +23,11 @@ export const navLinks = [
 
 export const myProjects = [
   {
-    title: "Podcastr - AI Podcast Platform",
-    desc: "Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.",
+    title: "iPhone Website",
+    desc: " An interactive landing page showcasing the iPhone 15 Pro with dynamic animations and 3D model rendering for an immersive user experience.",
     subdesc:
-      "Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.",
-    href: "https://www.youtube.com/watch?v=zfAb95tJvZQ",
+      "Built using React, Three.js, GSAP, and TailwindCSS, the page enables users to explore the iPhone in various colors and sizes with smooth transitions. The website features a custom video carousel for a detailed product preview and is fully responsive for seamless accessibility across devices.",
+    href: "https://i-phone-project.vercel.app/#highlights",
     texture: "/textures/project/project1.mp4",
     logo: "/assets/project-logo1.png",
     logoStyle: {
@@ -49,22 +49,32 @@ export const myProjects = [
       },
       {
         id: 3,
-        name: "TypeScript",
-        path: "/assets/typescript.png",
+        name: "JavaScript",
+        path: "/assets/javascript.png",
       },
       {
         id: 4,
         name: "Framer Motion",
         path: "/assets/framer.png",
       },
+      {
+        id: 5,
+        name: "gsap",
+        path: "/assets/gsap.png",
+      },
+      {
+        id: 6,
+        name: "Three.js",
+        path: "/assets/threejs.png",
+      },
     ],
   },
   {
-    title: "LiveDoc - Real-Time Google Docs Clone",
-    desc: "LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.",
+    title: "TaskOrbit - Employee Management System",
+    desc: "A task management platform designed to streamline employee workflows with role-based access control and real-time task updates.",
     subdesc:
-      "With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.",
-    href: "https://www.youtube.com/watch?v=y5vE8y_f_OM",
+      "The system features separate dashboards for employees and admins with authentication and role management. React and Context API handle centralized state management, ensuring smooth data flow. Employees can dynamically create, assign, and track tasks, with session persistence maintained through local storage for an uninterrupted workflow experience.",
+    href: "https://employee-management-system-nine-xi.vercel.app/",
     texture: "/textures/project/project2.mp4",
     logo: "/assets/project-logo2.png",
     logoStyle: {
@@ -86,22 +96,22 @@ export const myProjects = [
       },
       {
         id: 3,
-        name: "TypeScript",
-        path: "/assets/typescript.png",
+        name: "Framer Motion",
+        path: "/assets/framer.png",
       },
       {
         id: 4,
-        name: "Framer Motion",
-        path: "/assets/framer.png",
+        name: "D3.js",
+        path: "/assets/D3.png",
       },
     ],
   },
   {
-    title: "CarePulse - Health Management System",
-    desc: "An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.",
+    title: "Rejouice - Dynamic Web Experience ",
+    desc: " A feature-rich, visually immersive web experience designed to enhance user engagement with smooth scrolling animations and interactive elements.",
     subdesc:
-      "With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.",
-    href: "https://www.youtube.com/watch?v=lEflo_sc82g",
+      "Using GSAP for advanced animations and LocomotiveJS for seamless scrolling, the project creates a highly interactive browsing experience. SwiperJS was integrated to provide an intuitive content carousel, ensuring dynamic content display. The fully responsive design ensures optimal performance across various screen sizes.",
+    href: "https://rejoice-website.vercel.app/",
     texture: "/textures/project/project3.mp4",
     logo: "/assets/project-logo3.png",
     logoStyle: {
@@ -115,8 +125,8 @@ export const myProjects = [
     tags: [
       {
         id: 1,
-        name: "React.js",
-        path: "/assets/react.svg",
+        name: "Javascript",
+        path: "/assets/javascript.png",
       },
       {
         id: 2,
@@ -124,27 +134,22 @@ export const myProjects = [
         path: "assets/tailwindcss.png",
       },
       {
-        id: 3,
-        name: "TypeScript",
-        path: "/assets/typescript.png",
-      },
-      {
         id: 4,
-        name: "Framer Motion",
-        path: "/assets/framer.png",
+        name: "3.js",
+        path: "/assets/D3.png",
       },
     ],
   },
   {
-    title: "Horizon - Online Banking Platform",
-    desc: "Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.",
+    title: "Nike Landing Page",
+    desc: " A modern and engaging landing page built to showcase Nike’s brand with sleek animations and interactive UI elements.",
     subdesc:
-      "Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.",
-    href: "https://www.youtube.com/watch?v=PuOVqP_cjkE",
+      "Developed using React, GSAP, and TailwindCSS, the page includes smooth page transitions and visually appealing animations for an enhanced user experience. The clean, minimalist design, along with responsive layout optimization, ensures fast performance and seamless accessibility across all devices.",
+    href: "https://nike-inky-chi.vercel.app/",
     texture: "/textures/project/project4.mp4",
     logo: "/assets/project-logo4.png",
     logoStyle: {
-      backgroundColor: "#0E1F38",
+      backgroundColor: "#808080",
       border: "0.2px solid #0E2D58",
       boxShadow: "0px 0px 60px 0px #2F67B64D",
     },
@@ -152,8 +157,8 @@ export const myProjects = [
     tags: [
       {
         id: 1,
-        name: "React.js",
-        path: "/assets/react.svg",
+        name: "Javascript",
+        path: "/assets/javascript.png",
       },
       {
         id: 2,
@@ -162,45 +167,8 @@ export const myProjects = [
       },
       {
         id: 3,
-        name: "TypeScript",
-        path: "/assets/typescript.png",
-      },
-      {
-        id: 4,
-        name: "Framer Motion",
-        path: "/assets/framer.png",
-      },
-    ],
-  },
-  {
-    title: "Imaginify - AI Photo Manipulation App",
-    desc: "Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.",
-    subdesc:
-      "Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.",
-    href: "https://www.youtube.com/watch?v=Ahwoks_dawU",
-    texture: "/textures/project/project5.mp4",
-    logo: "/assets/project-logo5.png",
-    logoStyle: {
-      backgroundColor: "#1C1A43",
-      border: "0.2px solid #252262",
-      boxShadow: "0px 0px 60px 0px #635BFF4D",
-    },
-    spotlight: "/assets/spotlight5.png",
-    tags: [
-      {
-        id: 1,
-        name: "React.js",
-        path: "/assets/react.svg",
-      },
-      {
-        id: 2,
-        name: "TailwindCSS",
-        path: "assets/tailwindcss.png",
-      },
-      {
-        id: 3,
-        name: "TypeScript",
-        path: "/assets/typescript.png",
+        name: "JavaScript",
+        path: "/assets/javascript.png",
       },
       {
         id: 4,
@@ -217,36 +185,3 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
     deskPosition: isMobile ? [0.5, -5, 0] : [0.25, -6.5, 0],
   };
 };
-
-export const workExperiences = [
-  {
-    id: 1,
-    name: "Framer",
-    pos: "Lead Web Developer",
-    duration: "2022 - Present",
-    title:
-      "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
-    icon: "/assets/framer.svg",
-    animation: "victory",
-  },
-  {
-    id: 2,
-    name: "Figma",
-    pos: "Web Developer",
-    duration: "2020 - 2022",
-    title:
-      "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
-    icon: "/assets/figma.svg",
-    animation: "clapping",
-  },
-  {
-    id: 3,
-    name: "Notion",
-    pos: "Junior Web Developer",
-    duration: "2019 - 2020",
-    title:
-      "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
-    icon: "/assets/notion.svg",
-    animation: "salute",
-  },
-];
