958f9701e15b24fa41df30250ae02b7b53a83a3e
bhuvan <bhuvanannappa@gmail.com>
2025-02-07 21:55:04 +0530

    changed named import

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { navLinks } from "../constants/index.js";
-import useSmoothScroll from "../components/UseSmoothScroll.jsx";
+import UseSmoothScroll from "../components/UseSmoothScroll.jsx";
 
 const NavItems = ({ onClick = () => {} }) => {
   const scrollTo = useSmoothScroll();
