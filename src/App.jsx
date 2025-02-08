0368d221b25cdad0ea74abd5e0d6017ca5e57393
bhuvan <bhuvanannappa@gmail.com>
2025-02-08 14:12:18 +0530

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Project from "./sections/Project";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <About />
       <Project />
+      <Footer />
     </main>
   );
 };
