de258f192b9148d7f3d9266e04a25ce27f880f88
bhuvan <bhuvanannappa@gmail.com>
2025-02-08 16:48:29 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Project from "./sections/Project";
-import Footer from "./section/Footer";
+import Footer from "./sections/Footer";
 import Contact from "./sections/Contact";
 
 const App = () => {
